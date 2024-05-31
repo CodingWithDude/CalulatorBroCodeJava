73425b5938a2aee91efffaeca263e1345f5a7d13
codingwithdude <codingwithdude@gmail.com>
2024-05-31 11:07:13 -0700

    Setup Calculator Class and inital JFrame

---
+++ CalulatorBroCodeJava/src/Calculator.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton additionButton, subtractionButton, multiplicationButton, DivisionButton;
+    JButton decimalButton, equalsButton, deleteButton, clearButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    Calculator() {
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+
+        
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
