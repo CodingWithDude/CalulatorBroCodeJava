0233cd8f28a248ed8031abb4f320cc14a8ef0cbf
codingwithdude <codingwithdude@gmail.com>
2024-05-31 11:26:11 -0700

    Added number and function buttons updated actionPerformed for numberButtons

--- CalulatorBroCodeJava/src/Calculator.java
+++ CalulatorBroCodeJava/src/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator implements ActionListener {
     JTextField textField;
     JButton[] numberButtons = new JButton[10];
     JButton[] functionButtons = new JButton[8];
-    JButton additionButton, subtractionButton, multiplicationButton, DivisionButton;
+    JButton additionButton, subtractionButton, multiplicationButton, divisionButton;
     JButton decimalButton, equalsButton, deleteButton, clearButton;
     JPanel panel;
 
@@ -26,7 +26,72 @@ public class Calculator implements ActionListener {
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
 
-        
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        additionButton = new JButton("+");
+        subtractionButton = new JButton("-");
+        multiplicationButton = new JButton("*");
+        divisionButton = new JButton("/");
+        decimalButton = new JButton(".");
+        equalsButton = new JButton("=");
+        deleteButton = new JButton("Delete");
+        clearButton = new JButton("Clear");
+
+        functionButtons[0] = additionButton;
+        functionButtons[1] = subtractionButton;
+        functionButtons[2] = multiplicationButton;
+        functionButtons[3] = divisionButton;
+        functionButtons[4] = decimalButton;
+        functionButtons[5] = equalsButton;
+        functionButtons[6] = deleteButton;
+        functionButtons[7] = clearButton;
+
+        for (int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+            numberButtons[i].addActionListener(this);
+        }
+
+        deleteButton.setBounds(50, 430, 145, 50);
+        clearButton.setBounds(205, 430, 145, 50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        //panel.setBackground(Color.GRAY);
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(additionButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subtractionButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(multiplicationButton);
+        panel.add(decimalButton);
+        panel.add(numberButtons[0]);
+        panel.add(equalsButton);
+        panel.add(divisionButton);
+
+
+        frame.add(panel);
+        frame.add(deleteButton);
+        frame.add(clearButton);
+        frame.add(textField);
         frame.setVisible(true);
 
     }
@@ -40,5 +105,10 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
     }
 }
